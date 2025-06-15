8c4ad6e18af8146ba9afe4a70095af2e4c6c8329
Olaitan Kehinde <105742731+alvicci@users.noreply.github.com>
2025-06-15 10:05:30 +0100

    added the functions for the calculation and create another function to optimize the operation when clicked

---
+++ calculator.js
@@ -0,0 +1,38 @@
+"use script";
+
+const addEl = document.getElementById("add");
+const subtractEl = document.getElementById("subtract");
+const multiplyEl = document.getElementById("multiply");
+const divideEl = document.getElementById("divide");
+
+const calculationResult = document.getElementById("calculation-result");
+
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+function substract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 + number2;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  addEl.addEventListener("click", () => performOperation(add));
+  subtractEl.addEventListener("click", () => performOperation(substract));
+multiplyEl.addEventListener("click", () => performOperation(add));
+divideEl.addEventListener("click", () => performOperation(add));
+});
+
+const performOperation = (func) => {
+  const num1 = parseFloat(document.getElementById("number1").value) || 0;
+  const num2 = parseFloat(document.getElementById("number2").value) || 0;
+  const result = func(num1, num2);
+  calculationResult.textContent = result;
+};
